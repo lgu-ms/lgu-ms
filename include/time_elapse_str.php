e9995583853144915ba897036ce83ff7e49055e9
Melvin Jones Repol <mrepol742@gmail.com>
2023-11-24 10:55:12 +0800

    Initial Commit

---
+++ include/time_elapse_str.php
@@ -0,0 +1,29 @@
+<?php
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->m = floor($diff->d / 28);
+    $diff->d -= $diff->m * 28;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+?>
